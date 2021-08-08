66670981124270c92c71642d4ba9b5e092b09aa8
unicorno <jasurbeknorbekov@gmail.com>
2021-08-08 11:07:45 +0500

    feat: add function compareTriplets

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
   setup(props) {
-    var isAnagram = function (s, t) {
-      for(let i = 0; i < s.length; i++) {
-        
-        console.log(s[i]);
+    function compareTriplets(a, b) {
+      // Write your code here
+      const answer = [0, 0]
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+          answer[0]++
+        } else if (a[i] < b[i]) {
+          answer[1]++
+        }
       }
+      return answer
     }
-    isAnagram('anagram', 'nagaram')
+
+    compareTriplets([17, 28, 30], [99, 16, 8])
   },
 }
 </script>
