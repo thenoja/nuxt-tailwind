ed6d5492849be05b3a5a93a83ae653bbad195067
unicorno <jasurbeknorbekov@gmail.com>
2021-08-08 22:23:15 +0500

    minor changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,42 @@
 <template>
-  <div></div>
+  <div class="text-gray-600 grid grid-cols-1 md:grid-cols-2 gap-2 ">
+    <div class='text-right '>
+      <nav>
+        <a href="#">
+          <h1 class="text-gray-700 uppercase font-semibold p-4 border-b border-blue-500 inline-block mb-4">food ninja</h1>
+        </a>
+        <ul class='p-2'>
+          <li>
+            <a href="#">
+              <span class="font-bold">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>About</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Contact</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class='bg-gray-100'>
+      <div class="flex">
+        <div class='p-4'>Login</div>
+        <div class='py-1 leading-3 px-3 border-4 border-red-300 rounded-2xl grid place-items-center'>Logout</div>
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
 export default {
   setup(props) {
-    function compareTriplets(a, b) {
-      // Write your code here
-      const answer = [0, 0]
-      for (let i = 0; i < a.length; i++) {
-        if (a[i] > b[i]) {
-          answer[0]++
-        } else if (a[i] < b[i]) {
-          answer[1]++
-        }
-      }
-      return answer
-    }
-
-    compareTriplets([17, 28, 30], [99, 16, 8])
   },
 }
 </script>
